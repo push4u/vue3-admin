da0488c5b75da60a3ab1c0199c1ad2cf71641f04
push4u <1595898735@qq.com>
2024-03-05 23:10:08 +0800

    fix: 按钮权限控制重复请求接口

--- src/directives/permission/index.ts
+++ src/directives/permission/index.ts
@@ -1,15 +1,18 @@
-import { type Directive, nextTick } from "vue"
+import { Directive, nextTick } from "vue"
 import { usePermissionStoreHook } from "@/store/modules/permission"
 
 /** 权限指令 */
 export const permission: Directive = {
   async mounted(el, binding) {
     const permissionStore = usePermissionStoreHook()
-    const permissions = permissionStore.apiPermissionMap
-    // 判断map是否为空
+    let permissions = permissionStore.apiPermissionMap
+
+    // 如果没有权限信息，那么获取权限信息
     if (Object.keys(permissions).length === 0) {
       await permissionStore.fetchPermissions()
+      permissions = permissionStore.apiPermissionMap
     }
+
     await nextTick() // 等待 DOM 更新完成
     if (!permissions[binding.value]) {
       el.style.display = "none"
