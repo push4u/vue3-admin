ca9ed29693430a283922cfbf7310de140c01ae77
push4u <1595898735@qq.com>
2024-03-05 19:37:32 +0800

    feat: 用户修改不能修改用户名

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -326,9 +326,6 @@ watch([() => paginationData.currentPage, () => paginationData.pageSize], getTabl
         label-width="100px"
         label-position="right"
       >
-        <el-form-item prop="username" label="用户名">
-          <el-input v-model="formData.username" placeholder="请输入" />
-        </el-form-item>
         <el-form-item prop="password" label="密码">
           <el-input v-model="formData.password" placeholder="请输入" />
         </el-form-item>
